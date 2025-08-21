bdf05b2a0efb362a37daa2f39f3d8d4b18872524
ApoorvaPoojary8 <apoorvapoojary8@gmail.com>
2025-08-21 21:10:10 +0530

    conversion of datatypes

---
+++ 01_Basics/03_convertion.js
@@ -0,0 +1,30 @@
+//conversion of data types in JavaScript
+let score = "33abc"
+
+console.log(typeof score);//  number
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+// "33" => number
+// "33abc" => NaN (Not a Number)
+// "abc" =>NaN
+// undefined => NaN
+// null => 0
+// true => 1
+// false => 0
+
+let isLoggedIn = 1
+
+let isLoggedInBoolean = Boolean(isLoggedIn)
+ 
+console.log(isLoggedInBoolean)
+
+// 1 => true;0 =>
+// "" => false
+//"Apoorva" = >true
+
+let someNumber = 33
+let stringNumber =String(someNumber)
+console.log(typeof stringNumber) //string
