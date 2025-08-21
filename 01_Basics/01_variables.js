9c6b5fec7664bd0af5acd4368bcf3082ba807516
ApoorvaPoojary8 <apoorvapoojary8@gmail.com>
2025-08-21 19:14:04 +0530

    datatypes file is added

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14445
+let accountEmail = "apoorva9@gmail.com"
+var accountPassword = "pas123"
+accountCity = "Mangaluru"
+
+// accountId = 2 //this is not allowed as accountId is a constant
+accountEmail = "apoorva8@gmail.com"
+accountPassword = "pas456" 
+accountCity = "Bengaluru"
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";// treat all Js code as new versionECMAScript 5
+
+//alert(3+3) //we are using nodejs not browser
+//code readabilty should be good
+console.log(3+3);
+console.log("hello World")
+
+
+let name = "Apoorva"//
+let age = 20
+let isLoggedIn = false
+let state;
+
+//number =>2 to power 53
+//bigInt => 2 to power 53 + 1
+//String => "Apoorva"
+//Boolean => true/false
+//null => empty value or standalone value
+//undefined => variable is not defined
+//Symbol => unique value
+console.log(typeof name)
+
+console.log(typeof null) //object
+console.log(typeof undefinded) //undefined
